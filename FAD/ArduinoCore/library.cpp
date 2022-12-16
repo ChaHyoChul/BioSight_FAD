1da6d11587612612092f8075378b3b50b3e06f14
rndybkim <ybkim@rnd.re.kr>
2022-12-16 11:35:36 +0900

    Initial Commit

---
+++ FAD/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2022-12-16 오전 11:27:06
+ * Author : perso
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
